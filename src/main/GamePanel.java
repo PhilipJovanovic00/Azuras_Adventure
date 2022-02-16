7c8a0f05f1e82320cc3a1047ae0e65514c1b852a
Philip <jovanovicp@bzz.ch>
2022-02-16 01:19:46 +0100

    Changed else if in gamepanel to if, now you can move on the diagonal line. created little package debug, just to have some useful code, in case i need it for some debugging. created package Entity, in this package all npcs and the player will be stored. the movement, so the update and draw method have been moved to Player.java the color and rectangle are now made in the player class, which makes sense, because this will later be the player not just a white rectangle. Next step will probably be sprites.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,24 +11,36 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;  // 16 x 16 Tiles
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48 x 48 Tiles
+    public final int tileSize = originalTileSize * scale; // 48 x 48 Tiles
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 Pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 Pixels
 
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    //set players default position on map
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
 
-    public GamePanel(){
+    public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.decode("#000000"));
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
 
     }
-    public void startGameThread(){
+
+    public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
@@ -34,36 +48,56 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0; // <-- could be removed theoretically
+
 
         while (gameThread != null) {
-//            System.out.println("The Loop is running");
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime); // <-- could be removed theoretically
+            lastTime = currentTime;
 
             // 1 Update: update information such as character positions
-            update();
 
             // 2 Draw: Draw the screen with the updated information
-            repaint();
 
-        }
-    }
-        public void update() {
-
-        }
-        public void paintComponent(Graphics g){
-
-            super.paintComponent(g);
-
-            Graphics2D g2 = (Graphics2D)g;
-
-            g2.setColor(Color.decode("#ffffff"));
-
-            g2.fillRect(100, 100, tileSize, tileSize);
-
-            g2.dispose();
-
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
 
         }
 
 
     }
+
+    public void update() {
+
+        player.update();
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        player.draw(g2);
+
+        g2.dispose();
+
+
+    }
+
+
+}
 
---
+++ src/selfDebug/Debug.java
@@ -0,0 +1,20 @@
+package selfDebug;
+
+public class Debug {
+
+
+
+    //if (delta >= 1) {
+    //  update();
+    //repaint();
+    //delta--;
+    //drawCount++;
+
+    //this here is for showing the FPS, might be important, if the game is getting really laggy.
+
+    //if (timer >= 1000000000){
+    //    System.out.println("FPS: " + drawCount);
+    //    drawCount = 0;
+    //    timer = 0;
+
+}
