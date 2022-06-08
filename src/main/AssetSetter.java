2a9053f2f12e34640a43d9c5fc1704fa3db705ab
Philip Jovanovic <jovanovicp@bzz.ch>
2022-06-08 08:56:06 +0200

    Added new Object

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,5 +1,6 @@
 package main;
 
+import object.OBJ_Boot;
 import object.OBJ_Chest;
 import object.OBJ_Door;
 import object.OBJ_Key;
@@ -48,6 +49,10 @@ public class AssetSetter {
         gp.obj[6] = new OBJ_Chest();
         gp.obj[6].worldX = 10 * gp.tileSize;
         gp.obj[6].worldY = 7 * gp.tileSize;
+
+        gp.obj[7] = new OBJ_Boot();
+        gp.obj[7].worldX = 36 * gp.tileSize;
+        gp.obj[7].worldY = 41 * gp.tileSize;
     }
 
 }
